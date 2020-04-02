454ad30c05364e28f8846217dffe3415f064cbd5
Øystein Haug Olsen <oholsen@gmail.com>
2020-04-02 23:15:54 +0200

    Add remote control from GUI

--- src/python/AlphaBot.py
+++ src/python/AlphaBot.py
@@ -1,10 +1,16 @@
-import time
 import asyncio
-from PID import PID
-import pigpio
+import json
+import logging
+import time
 
-CNTL = 7 # CE1
-CNTR = 8 # CE0
+import pigpio
+from PID import PID
+
+log = logging.getLogger(__name__)
+pi = pigpio.pi()
+
+CNTL = 7  # CE1
+CNTR = 8  # CE0
 
 MLIN1 = 12
 MLIN2 = 13
@@ -14,30 +20,21 @@ MRIN1 = 20
 MRIN2 = 21
 MREN = 26
 
-Servo1Pin = 27 # pitch neg up
-Servo2Pin = 22 # yaw neg left
-ServoTiltPin = 27
-ServoPanPin = 22
-
+ServoTiltPin = 27  # tilt neg up
+ServoPanPin = 22  # pan neg left
 
 # Infrared detectors
 DR = 16
 DL = 19
 
 
-pi = pigpio.pi()
-
-
 class Proximity(object):
-        
+
     def __init__(self, pin):
         self.pin = pin
         pi.set_pull_up_down(pin, pigpio.PUD_UP)
-        #self.cb = pi.callback(pin, pigpio.RISING_EDGE) #, cb)
+        # self.cb = pi.callback(pin, pigpio.RISING_EDGE) #, cb)
 
-    def _cb(self, gpio, level, tick):
-        sys.out.write('!')
-            
     def isClose(self):
         return pi.read(self.pin) == 0
 
@@ -45,32 +42,36 @@ class Proximity(object):
 proximityLeft = Proximity(DL)
 proximityRight = Proximity(DR)
 
+
+def clip2(x, lo, hi):
+    return min(max(x, lo), hi)
+
+
 class Servo(object):
 
-    def __init__(self, pin, angle = 0, angleRange = 90, pulseWidthRange = 500, centerAngle = 0):
-        self.centerAngle = centerAngle
-        self.angleRange = angleRange
+    def __init__(self, pin, angle: float = 0, angleRange: float = 60, pulseWidthRange: float = 500):
+        self.angleRange = float(angleRange)
         self.pulseWidthRange = pulseWidthRange
         self.pin = pin
         pw = self._pulseWidth(angle)
+        print("pulse width", self.pin, pw)
         pi.set_servo_pulsewidth(self.pin, pw)
 
-    def _pulseWidth(self, angle):
+    def _pulseWidth(self, angle: float):
         # neutral is 1.5ms pulse
         # +-0.5ms is 90 degrees, depending on servo
-        angle += self.centerAngle
-        if angle > 0: angle = min(angle, self.angleRange)
-        else: angle = max(angle, -self.angleRange)
-        return 1500 + self.pulseWidthRange * angle / self.angleRange
-            
+        return 1500 + self.pulseWidthRange * clip2(angle / self.angleRange, -1, 1)
+
     def setAngle(self, angle):
         pw = self._pulseWidth(angle)
+        print("pulse width", self.pin, pw)
         pi.set_servo_pulsewidth(self.pin, pw)
 
     def stop(self):
+        print("pulse width", self.pin, 0)
         pi.set_servo_pulsewidth(self.pin, 0)
-                
-                
+
+
 class Motor(object):
 
     def __init__(self, in1, in2, en):
@@ -82,6 +83,7 @@ class Motor(object):
         pi.set_mode(self.IN2, pigpio.OUTPUT)
         pi.set_mode(self.EN, pigpio.OUTPUT)
 
+        self.power = 0
         self._stop()
         pi.set_PWM_frequency(self.EN, 500)
 
@@ -106,29 +108,29 @@ class Motor(object):
             self._forward()
         else:
             self._backward()
+        self.power = power
         pi.set_PWM_dutycycle(self.EN, min(255, int(255.0 * abs(power) / 100)))
 
-import sys
+
+"""
 def cb(gpio, level, tick):
         sys.stdout.write('!')
         sys.stdout.flush()
+"""
+
 
 class Counter(object):
-
     """Cannot detect slow speeds, depending on integration/count time"""
-    
+
     def __init__(self, pin):
-        self.cb = pi.callback(pin, pigpio.RISING_EDGE) #, cb)
-        #self.lastTime = time.time()
-        #self.lastCount = 0
+        self.cb = pi.callback(pin, pigpio.RISING_EDGE)  # , cb)
+        # self.lastTime = time.time()
+        # self.lastCount = 0
         self.alpha = 0.9
         self._speed = 0
         self.counts = [(time.time(), 0)]
         self.n = 4
 
-    def _cb(self, gpio, level, tick):
-        sys.out.write('!')
-            
     def speed(self):
         # alpha depends on update rate....
         # TODO: use time difference, automatic adjustment of alpha
@@ -149,52 +151,53 @@ class Counter(object):
                 break
         else:
             tlast, clast = self.counts[-1]
-                
+
         self.counts.insert(0, (t, count))
         while len(self.counts) > self.n:
             self.counts.pop()
 
-        #print self.counts
         dcount = count - clast
-        dt = t - tlast 
-        if 0:
-            print()
-            print("speed", count, dcount, dt)
-            print()
+        dt = t - tlast
         v = float(dcount) / dt
         self._speed = self.alpha * v + (1 - self.alpha) * self._speed
-        #return self._speed, v, dcount, dt
         return self._speed
 
     def count(self):
         return self.cb.tally()
-        
+
+
 class MotorController:
 
     def __init__(self, name, motor, counter):
         self.name = name
         self.motor = motor
         self.counter = counter
-        self.pid = PID(1, 0.3, 0, I_max=100, I_min=-100)
-        self.lastUpdateTime = time.time()
-            
-    def update(self, debug=False):
-        t = time.time()
-        if t - self.lastUpdateTime < 0.100:
-            return
-        
+        self.pid = PID(0.5, 0.3, 0, I_max=50, I_min=-50)
+
+    def update(self, t: float, debug=False):
+
+        # print("update", self.name, self.pid.getPoint())
         countRate = self.counter.speed()
-        #print self.counter.speed(), countRate, self.counter.times
+        # print self.counter.speed(), countRate, self.counter.times
+
+        # Don't update PID when the motor is stopped
+        if abs(self.pid.getPoint()) < 1:
+            self.motor.setPower(0)
+            self.pid.reset()
+            return
 
         # TODO: can set direction in counter
-        if self.pid.getPoint() < 0:
+        if self.motor.power < 0:
             countRate = -countRate
-                
-        power = self.pid.update(t, countRate)
-        if power is None: return
 
+        power = self.pid.update(t, countRate)
+        # print("update power", self.name, power)
+        if power is None:
+            return
+
+        power = -power
         if debug:
-            print('%s  %6.2f %6.2f  %6.2f  %6.2f  %6.2f' % (self.name, countRate, self.pid.P, self.pid.I, self.pid.D, power), end=' ') 
+            print('%s  %6.2f %6.2f  %6.2f  %6.2f  %6.2f' % (self.name, countRate, self.pid.P, self.pid.I, self.pid.D, power), end=' ')
         if power > 0:
             power = min(power, 100)
         else:
@@ -204,142 +207,210 @@ class MotorController:
             print(' %6.2f' % power)
 
     def setSpeed(self, speed):
-         self.pid.setPoint(time.time(), speed)
+        self.pid.setPoint(speed)
 
     def getSpeed(self):
-         return self.pid.getPoint()
-            
+        return self.pid.getPoint()
+
     def stop(self):
         self.setSpeed(0)
         self.motor.stop()
 
+    def status(self):
+        return {
+            "power": self.motor.power,
+            "setSpeed": self.getSpeed(),
+            "speed": self.counter.speed(),
+            "pid": self.pid.status(),
+        }
 
 
 class AlphaBot():
 
     def __init__(self):
-        self.cameraPan = Servo(ServoPanPin, centerAngle = 25)
-        self.cameraTilt = Servo(ServoTiltPin, 5)
-        self.left  = MotorController('L', Motor(MLIN1, MLIN2, MLEN), Counter(CNTL))
+        self.cameraPan = Servo(ServoPanPin)
+        self.cameraTilt = Servo(ServoTiltPin)
+        self.left = MotorController('L', Motor(MLIN1, MLIN2, MLEN), Counter(CNTL))
         self.right = MotorController('R', Motor(MRIN2, MRIN1, MREN), Counter(CNTR))
-        self.setSpeed(0)
+        self.speed: float = 0
+        self.turnSpeed: float = 0
+        self.stopped = True
 
     def stop(self):
-        print("stop")
+        log.info("stop")
+        self.stopped = True
+        self.speed = 0
+        self.turnSpeed = 0
         self.left.stop()
         self.right.stop()
         self.cameraPan.stop()
         self.cameraTilt.stop()
 
+    def status(self):
+        return {
+            "stopped": self.stopped,
+            # "proxLeft": proximityLeft.isClose(),
+            # "proxRight": proximityRight.isClose(),
+            "motorLeft": self.left.status(),
+            "motorRight": self.right.status(),
+            "speed": self.speed,
+            "turn": self.turnSpeed,
+        }
+
     def isClose(self):
         return proximityLeft.isClose() or proximityRight.isClose()
-    
-    def setSpeed(self, speed):
-        print("speed", speed)
-        self.left.setSpeed(speed)
-        self.right.setSpeed(speed)
-        
-    def getSpeed(self):
+
+    def setSpeed(self, speed: float):
+        log.info("speed %g", speed)
+        self.speed = speed
+        self._command()
+
+    def setTurn(self, turn: float):
+        log.info("turn %g", turn)
+        self.turnSpeed = turn
+        self._command()
+
+    def getSpeed(self) -> float:
         return 0.5 * (self.left.getSpeed() + self.right.getSpeed())
-        
-    def setTurnSpeed(self, speed, turnSpeed):
-        print("turn", speed, turnSpeed)
-        self.left.setSpeed(speed + turnSpeed)
-        self.right.setSpeed(speed - turnSpeed)
-        
+
+    def setSpeedAndTurn(self, speed: float, turnSpeed: float):
+        log.info("speedturn %g %g", speed, turnSpeed)
+        self.speed = speed
+        self.turnSpeed = turnSpeed
+        self._command()
+
+    def _command(self):
+        log.debug("_command %g %g", self.speed, self.turnSpeed)
+        self.stopped = False
+        self.left.setSpeed(self.speed + self.turnSpeed)
+        self.right.setSpeed(self.speed - self.turnSpeed)
+
     def update(self):
-        self.left.update()
-        self.right.update()
-        
-        
+        if not self.stopped:
+            t = time.time()
+            self.left.update(t)
+            self.right.update(t)
+
+
 alphabot = AlphaBot()
 
 
 async def controlLoop(dt):
     while True:
         await asyncio.sleep(dt)
-        #print(alphabot.isClose(), alphabot.getSpeed())        
-        if alphabot.isClose() and alphabot.getSpeed() > 0:
-            # set speed here and not in task to avoid multiple tasks
-            alphabot.setSpeed(-20)
-            loop.create_task(avoidObstacle(20))
-        alphabot.update()
+        try:
+            # print(alphabot.isClose(), alphabot.getSpeed())        
+            if 0:
+                # automatic obstacle avoidance
+                if alphabot.isClose() and alphabot.speed > 0:
+                    # set speed here and not in task to avoid multiple tasks
+                    alphabot.setSpeed(-20)
+                    loop.create_task(avoidObstacle(20))
+            alphabot.update()
+        except:
+            log.exception("Control loop")
 
 
 async def avoidObstacle(speed):
-    print("avoidObstacle")
-
+    log.info("avoidObstacle")
     await asyncio.sleep(2.5)
-
-    alphabot.setTurnSpeed(0, 20)
+    alphabot.setSpeedAndTurn(0, 20)
     await asyncio.sleep(1.5)
-
     alphabot.setSpeed(speed)
 
 
 async def speedLoop(speed):
-
     await asyncio.sleep(2)
-    
     while True:
-        
         alphabot.setSpeed(speed)
         break
-        
         await asyncio.sleep(5)
-        
         alphabot.setSpeed(-speed)
         await asyncio.sleep(5)
 
 
+def handle(message):
+    try:
+        log.info("Handle %r", message)
+        cols = message.split()
+        cmd = cols.pop(0)
+        if cmd == "!" or cmd == ".":
+            # ! is reset, . is stop
+            alphabot.stop()
+        elif cmd == "t":
+            alphabot.setSpeed(float(cols[0]))
+        elif cmd == "r":
+            alphabot.setTurn(float(cols[0]))
+        elif cmd == "pan:":
+            alphabot.cameraPan.setAngle(float(cols[0]))
+        elif cmd == "tilt":
+            alphabot.cameraTilt.setAngle(float(cols[0]))
+        else:
+            log.warning("Ignore message %r", message)
+    except:
+        log.exception("Handle message %r", message)
+
+
+clients = set()
+
+
 async def wscontrol(websocket, path):
-    import json
+    clients.add(websocket)
+    try:
+        async for message in websocket:
+            handle(message)
+    except Exception as e:
+        log.exception("websocket: %s", e)
+    finally:
+        clients.remove(websocket)
+
+
+async def post(msg):
+    if clients:
+        await asyncio.wait([client.send(msg) for client in clients])
+
+
+async def status():
     while True:
-        message = await websocket.recv()
+        await asyncio.sleep(0.2)
         try:
-            result = eval(message)
-            await websocket.send(json.dumps(result))
+            status = alphabot.status()
+            await post(json.dumps(status))
         except Exception as e:
-            print(str(e))
-            #print(json.dumps(e))
-            await websocket.send(json.dumps(str(e)))
+            log.exception("status: %s", e)
 
 
 if __name__ == '__main__':
 
+    import argparse
     import websockets
 
-
-    #testServo(Servo1Pin, -30, +30, 5)
-    #testServo(Servo2Pin, -90, +90)
-    #testMotor()
-    #testMotorCounter()
-    #testProximity()
-            
-    import argparse
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('speed', type=float)
+    # parser.add_argument('speed', type=float)
     args = parser.parse_args()
-    print(args.speed)
-    #testSpeed(args.speed)
 
-    tStart = time.time()    
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger("websockets").setLevel(logging.INFO)
+
+    # log.info("Speed %f", args.speed)
+    # testSpeed(args.speed)
+
+    tStart = time.time()
     loop = asyncio.get_event_loop()
 
     try:
         # alphabot.setSpeed(args.speed)
         tasks = [
             asyncio.Task(controlLoop(0.050)),
-            asyncio.Task(speedLoop(args.speed)),
-            websockets.serve(wscontrol, '0.0.0.0', 8765)
+            asyncio.Task(status()),
+            # asyncio.Task(speedLoop(args.speed)),
+            websockets.serve(wscontrol, '0.0.0.0', 8000)
         ]
         loop.run_until_complete(asyncio.wait(tasks))
         loop.run_forever()
-        print("Loop complete")
+        log.info("Loop complete")
     finally:
         loop.stop()
         loop.close()
         alphabot.stop()
         pi.stop()
-
--- src/python/PID.py
+++ src/python/PID.py
@@ -3,70 +3,80 @@ class PID:
     Discrete PID control
     """
 
-    def __init__(self, Kp=1.0, Ti=0, Td=0, I_max=1, I_min=-1):
+    def __init__(self, Kp: float = 1.0, Ti: float = 0, Td: float = 0, I_max: float = 1, I_min: float = -1, error_alpha: float = 0.1):
 
-        self.Kp = Kp
-        self.Ti = Ti
-        self.Td = Td
+        # parameters
+        self.Kp: float = Kp
+        self.Ti: float = Ti
+        self.Td: float = Td
+        self.I_max: float = I_max
+        self.I_min: float = I_min
+        self.error_alpha = error_alpha
 
-        self.I = 0 # None
-        self.I_max = I_max
-        self.I_min = I_min
-        
-        self.set_point = None
-        self.error = None
+        # state
+        self.P: float = 0
+        self.I: float = 0
+        self.D: float = 0
+        self.derror_dt: float = 0
+        self.set_point: float = 0
+        self.lastUpdateTime: float = None
+        self.lastError: float = None
+
+    def reset(self):
+        self.P = 0
+        self.I = 0
+        self.D = 0
+        self.derror_dt = 0
         self.lastUpdateTime = None
 
+    def status(self):
+        return {
+            "P": self.P,
+            "I": self.I,
+            "D": self.D,
+            "dedt": self.derror_dt,
+        }
 
-    def update(self, t, current_value):
+    def update(self, t: float, current_value: float):
         """
         Calculate PID output value for given reference input and feedback
-
-        Ki is 1/tau
         """
 
-        # negative of error really
-        error = self.set_point - current_value
+        error = current_value - self.set_point
 
         if self.lastUpdateTime is None:
-            self.lastError = self.error
+            self.lastError = error
             self.lastUpdateTime = t
             return None
 
-        
         dt = t - self.lastUpdateTime
-        
+
         self.P = self.Kp * error
 
-        # TODO: smoothing of derivative, external calculation?
-        if self.error is None:
-            self.D = 0
-        else:
-            self.D = self.Td * (error - self.error) / dt
+        # TODO: use second order deriviate
+        derror_dt = (error - self.lastError) / dt
+        self.derror_dt += self.error_alpha * (derror_dt - self.derror_dt) / dt
+        self.D = self.Td * self.derror_dt
 
-        #print error, self.error, self.Kd, self.D
+        if self.Ti > 0:
+            self.I += self.P * dt / self.Ti
+            if self.I > self.I_max:
+                self.I = self.I_max
+            elif self.I < self.I_min:
+                self.I = self.I_min
 
-        self.I += self.P * dt / self.Ti
-
-        if self.I > self.I_max:
-            self.I = self.I_max
-        elif self.I < self.I_min:
-            self.I = self.I_min
-
-        self.error = self.P
         self.lastUpdateTime = t
+        self.lastError = error
 
         return self.P + self.I + self.D
 
-
-    def setPoint(self, t, set_point):
+    # FIXME: setSetPoint()
+    def setPoint(self, set_point):
         """
         Initialize the setpoint of PID
         """
         self.set_point = set_point
-        self.I = 0
-        self.lastError = None
-        self.lastUpdateTime = t
+        self.reset()
 
     def setKp(self, Kp):
         self.Kp = Kp
@@ -77,6 +87,6 @@ class PID:
     def setTd(self, Td):
         self.Td = Td
 
+    # FIXME: getSetPoint()
     def getPoint(self):
         return self.set_point
-
